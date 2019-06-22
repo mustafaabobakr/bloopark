86a20f6312c446143301e952f7122d9f348dd6dc
Mostafa Abobakr <mostafaabobakr777@gmail.com>
2019-06-22 19:54:59 +0200

    Updates

---
+++ precache-manifest.a5f407be27e7a2dd06d16863c3415ee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffe5568c2c00f45123a03ad728a4123d",
+    "url": "/bloopark/static/media/logo.ffe5568c.png"
+  },
+  {
+    "revision": "f4ec18f89f60d259c2ae",
+    "url": "/bloopark/static/js/runtime~main.f4ec18f8.js"
+  },
+  {
+    "revision": "668950147fd24463875a",
+    "url": "/bloopark/static/js/main.66895014.chunk.js"
+  },
+  {
+    "revision": "5b8a673576fe9146507d",
+    "url": "/bloopark/static/js/2.5b8a6735.chunk.js"
+  },
+  {
+    "revision": "668950147fd24463875a",
+    "url": "/bloopark/static/css/main.02fb0e92.chunk.css"
+  },
+  {
+    "revision": "e2e29b97a5a30c7364d35ba0182c046e",
+    "url": "/bloopark/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bloopark/precache-manifest.7e02d1f352ee8dd8271f59bd0499cc61.js"
+  "/bloopark/precache-manifest.a5f407be27e7a2dd06d16863c3415ee7.js"
 );
 
 workbox.clientsClaim();
