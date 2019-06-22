a3ba083fb435eabfeaa8ea08bc3ce68377331e2f
Mostafa Abobakr <mostafaabobakr777@gmail.com>
2019-06-22 17:54:44 +0200

    Updates

---
+++ precache-manifest.ab1512ce66d49dfc9aec04e67d8181b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffe5568c2c00f45123a03ad728a4123d",
+    "url": "/bloopark/static/media/logo.ffe5568c.png"
+  },
+  {
+    "revision": "f4ec18f89f60d259c2ae",
+    "url": "/bloopark/static/js/runtime~main.f4ec18f8.js"
+  },
+  {
+    "revision": "2ec73a519c4616c90f50",
+    "url": "/bloopark/static/js/main.2ec73a51.chunk.js"
+  },
+  {
+    "revision": "460319decc150e16dcfb",
+    "url": "/bloopark/static/js/2.460319de.chunk.js"
+  },
+  {
+    "revision": "2ec73a519c4616c90f50",
+    "url": "/bloopark/static/css/main.6a022296.chunk.css"
+  },
+  {
+    "revision": "dcd8d6461d45db354be0b43e091638dd",
+    "url": "/bloopark/index.html"
+  }
+];
